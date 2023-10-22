e8ecb3e544ca68252901ed4dc83711704ba25d6f
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2023-10-22 03:01:11 +0200

    Add tests for user.service

--- apps/auth/vite.config.ts
+++ apps/auth/vite.config.ts
@@ -1,12 +1,17 @@
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { defineConfig } from "vitest/config";
 
-export default {
+export default defineConfig({
   cacheDir: "../../node_modules/.vite/auth",
   plugins: [nxViteTsPaths()],
   test: {
     cache: {
       dir: "../../node_modules/.vitest",
     },
+    coverage: {
+      enabled: true,
+      reportsDirectory: "../../coverage/apps/auth",
+    },
     include: ["src/**/*.spec.ts"],
   },
-};
+});
