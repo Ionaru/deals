07d438a74ccdbdaa03a3c2929207fe4426435d4a
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2023-12-23 00:23:16 +0100

    Run format

--- apps/client/src/test-setup.ts
+++ apps/client/src/test-setup.ts
@@ -4,8 +4,8 @@ import "@angular/localize/init";
 // Fix for "ReferenceError: TextEncoder is not defined"
 // https://github.com/angular/angular/issues/48748
 // eslint-disable-next-line @typescript-eslint/no-shadow
-import { TextEncoder } from 'node:util'
-Object.defineProperty(global, 'TextEncoder', {
+import { TextEncoder } from "node:util";
+Object.defineProperty(global, "TextEncoder", {
   value: TextEncoder,
   writable: true,
 });
