c0e07fcc3414c69ea1b9201335d1b017d659a3b3
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2023-12-23 00:03:42 +0100

    Fix ReferenceError: TextEncoder is not defined: https://github.com/angular/angular/issues/48748#issuecomment-1385553666

--- apps/client/src/test-setup.ts
+++ apps/client/src/test-setup.ts
@@ -1,2 +1,11 @@
 import "jest-preset-angular/setup-jest";
 import "@angular/localize/init";
+
+// Fix for "ReferenceError: TextEncoder is not defined"
+// https://github.com/angular/angular/issues/48748
+// eslint-disable-next-line @typescript-eslint/no-shadow
+import { TextEncoder } from 'node:util'
+Object.defineProperty(global, 'TextEncoder', {
+  value: TextEncoder,
+  writable: true,
+});
