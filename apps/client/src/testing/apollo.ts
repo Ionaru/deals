54a7d88545bb5c8c2772c139222a9c7daabd0bce
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2024-02-20 23:25:06 +0100

    Remove unused images endpoint and test files

--- apps/client/src/testing/apollo.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloQueryResult, NetworkStatus } from "@apollo/client";
-
-export const buildApolloQueryResult = <T>(data: T): ApolloQueryResult<T> => ({
-  data,
-  loading: false,
-  networkStatus: NetworkStatus.ready,
-});
