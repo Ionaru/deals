757a5e6f346aef6e0cc665554b765e54df5bd568
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2024-11-05 13:27:22 +0100

    Add some timeout

--- apps/client-e2e/src/e2e/authentication.cy.ts
+++ apps/client-e2e/src/e2e/authentication.cy.ts
@@ -34,12 +34,12 @@ describe("Authentication", () => {
     cy.get("button").contains("Ik wil een nieuw account aanmaken").click();
     cy.get("mat-form-field").type("E2E Test User");
     cy.get("button").contains("Registreren").click();
-    cy.get("body").should("contain", "Account aangemaakt!");
+    cy.get("body", {timeout: 30_000}).should("contain", "Account aangemaakt!");
     cy.get("body").should("contain", "Name: E2E Test User");
     cy.get("button").contains("Inloggen").should("be.visible").click();
     cy.url().should("eq", Cypress.config().baseUrl + "/");
     cy.get(".signup-button").should("not.exist");
     cy.visit("/login");
-    cy.get("button").contains("Uitloggen").should("be.visible");
+    cy.get("button").contains("Uitloggen", {timeout: 30_000}).should("be.visible");
   });
 });
